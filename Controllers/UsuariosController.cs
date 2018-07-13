1ccec577861c4b4fcba5055e5520f13b223829f9
Elcio Júnior <elcio.junior@workshow.com.br>
2018-07-13 16:33:47 -0300

    Controller Usuarios post, get, put remove

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -31,5 +31,55 @@ namespace ApiCore.Controllers
             }
             return new ObjectResult(user);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Usuario usuario)
+        {
+            if (usuario == null)
+            {
+                return BadRequest("Usuário invalido");
+
+            }
+            _usuarioRepositorio.Add(usuario);
+
+            return CreatedAtRoute("GetUsuario", new { id = usuario.UsuarioID }, usuario);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] Usuario usuario)
+        {
+            if (usuario == null || usuario.UsuarioID != id)
+            {
+                return BadRequest("Usuário diferente");
+
+            }
+            var _user = _usuarioRepositorio.Find(id);
+
+            if (_user == null)
+            {
+                return NotFound("Usuário não encontrado");
+            }
+            _user.Email = usuario.Email;
+            _user.Nome = usuario.Nome;
+
+            _usuarioRepositorio.Update(_user);
+            return new NoContentResult();
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var user = _usuarioRepositorio.Find(id);
+
+            if (user == null)
+            {
+                return NotFound("Usuário não encontrado");
+            }
+            _usuarioRepositorio.Remove(id);
+            return new NoContentResult();
+
+        }
+
     }
 }
